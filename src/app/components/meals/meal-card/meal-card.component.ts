3962b44bb0dc83c722d4f61d20989200a1217906
fausto fuertes <fausto.fuertes.83@gmail.com>
2024-11-03 21:11:58 -0300

    boludes estetica

--- src/app/components/meals/meal-card/meal-card.component.ts
+++ src/app/components/meals/meal-card/meal-card.component.ts
@@ -1,19 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Food } from '../../../interfaces/food';
 import { FoodContainerComponent } from '../../food-container/food-container.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-meal-card',
   standalone: true,
-  imports: [FoodContainerComponent],
+  imports: [FoodContainerComponent, NgClass],
   templateUrl: './meal-card.component.html',
   styleUrl: './meal-card.component.css'
 })
 export class MealCardComponent {
   @Input() arrayFood?: Food[];
   @Output() deleteEmmiter = new EventEmitter();
+  isExpanded = false; // Estado del despliegue
 
   emitDelete(food: Food) {
     this.deleteEmmiter.emit(food);
+  }
+
+  toggleExpand() {
+    this.isExpanded = !this.isExpanded;
   }
 }
